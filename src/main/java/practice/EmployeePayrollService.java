ee031bfe9eed817ade3ce2fe0abb2ca699723c14
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-05-09 20:15:40 +0530

    Employee Payroll Service to retrieve

--- src/main/java/practice/EmployeePayrollService.java
+++ src/main/java/practice/EmployeePayrollService.java
@@ -3,38 +3,53 @@ package Practice;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Enumeration;
+
+import com.mysql.cj.xdevapi.Statement;
 
 public class EmployeePayrollService {
 	private static final String url = "jdbc:mysql://localhost:3306/employee_payroll";
 	private static final String userName = "root";
 	private static final String password = "Sanjana555@mhatre";
 
-	public void checkDatabaseConnection() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver loaded");
-			Connection con;
-			try {
-				con = DriverManager.getConnection(url, userName, password);
-				System.out.println("connected to database" + con);
-				listDrivers();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
 
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver loaded");
+		Connection connection = null;
+		connection = DriverManager.getConnection(url, userName, password);
+		System.out.println("connected to database" + connection);
+		return connection;
+
 	}
 
+	
+	public void checkDatabaseConnection() throws ClassNotFoundException, SQLException {
+		Connection connection = getConnection();
+		listDrivers();
+
+	}
+
+	
 	private void listDrivers() {
 		Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
-		while(driverList.hasMoreElements()) {
-			Driver driverClass = (Driver)driverList.nextElement();
-			System.out.println(" "+driverClass.getClass().getName());
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
 		}
+	}
+
+	
+	public void selectData() throws ClassNotFoundException, SQLException {
+		Connection connection = getConnection();
+		java.sql.Statement statement = connection.createStatement();
+		ResultSet resultSet = ((java.sql.Statement) statement).executeQuery("select * from employee_payroll");
+		while (resultSet.next()) {
+			System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " "
+					+ resultSet.getDouble(4) + " " + resultSet.getDate(5));
+		}
+
 	}
 }
--- src/test/java/practice/EmployeePayrollServiceTest.java
+++ src/test/java/practice/EmployeePayrollServiceTest.java
@@ -2,15 +2,21 @@ package practice;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 
 import Practice.EmployeePayrollService;
 
 public class EmployeePayrollServiceTest {
 	@Test
-	public void givenEmployeePayrollDatabase_ShouldConnectToDatabase() {
+	public void givenEmployeePayrollDatabase_ShouldConnectToDatabase() throws ClassNotFoundException, SQLException {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		employeePayrollService.checkDatabaseConnection();
 	}
-	
+	@Test
+	public void givenEmployeePayrollDatabase_ShouldRetrieveData() throws ClassNotFoundException, SQLException {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.selectData();
+	}
 }
